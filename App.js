a9bc4d1db54731c2ace36522149528540d6fef14
Neka <nekoanchor@yahoo.fr>
2021-10-05 01:45:43 +0200

    fix: wrong import & wrong version of packages

--- App.js
+++ App.js
@@ -4,14 +4,20 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import RecipesList from "./src/screens/RecipesList";
 import {Provider} from "react-redux";
 import {store} from "./src/redux/store.js";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaView style={styles.container}>
-        <RecipesList />
-        <StatusBar style="auto" />
-      </SafeAreaView>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="RecipesList" component={RecipesList} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
