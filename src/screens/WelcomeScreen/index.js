e821b4655ccf161b46bebe4ab6e72bfda3d46d10
Neka <nekoanchor@yahoo.fr>
2021-10-08 16:00:32 +0200

    feat: add welcome screen

---
+++ src/screens/WelcomeScreen/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {View, Card, FlatList, Text} from "react-native";
+
+export default function RecipesList(){
+
+  
+
+  return(
+    <>
+      <View styles={style.container}>
+        <Text styles={style.title}>Welcome</Text>
+        <Text styles={style.title}>Here is a small application in React Native linked to the Spoonacular API in order to display a recipe list.</Text>
+        <Text styles={style.title}>Move to the next screen</Text>
+      </View>
+    </>
+  )
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent:"center",
+    alignContent:"center"
+  },
+  title: {
+    fontWeight:"bold"
+  },
+})
